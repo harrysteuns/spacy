e7a0ddf539b853b16706870ade492b53ed439757
Harry <harry.steuns@gmail.com>
2019-08-16 23:17:57 +0200

    shuttleMouse comet falling

---
+++ Assets/Scripts/CometMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CometMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    Vector3 velocity = new Vector3(0, -2, 0);
+
+    void Update()
+    {
+        transform.position += velocity * Time.deltaTime;       
+    }
+}
---
+++ Assets/Scripts/CometSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CometSpawner : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject prefab;
+
+    [SerializeField]
+    private float spawnTime = 1.5f;
+    
+
+    private void Start()
+    {
+        StartCoroutine(CometSpawnRoutine());
+    }
+
+    private void SpawnComet()
+    {
+        GameObject comet = Instantiate(prefab);
+        
+    }
+
+    
+    private IEnumerator CometSpawnRoutine()
+    {
+        while (true)
+        {
+            SpawnComet();
+            yield return new WaitForSeconds(spawnTime);
+        }
+
+    }
+
+}
